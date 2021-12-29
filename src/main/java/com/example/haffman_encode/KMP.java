8e192f8e71aa5ee1aa301665c063720c09e126c9
SWQXDBA <983110853@qq.com>
2021-12-29 08:56:40 +0800

    1

--- src/main/java/com/example/haffman_encode/KMP.java
+++ src/main/java/com/example/haffman_encode/KMP.java
@@ -3,16 +3,7 @@ package com.example.haffman_encode;
 import java.util.Arrays;
 
 public class KMP {
-    public static void main(String[] args) {
-        String str1 = "BBC ABCDAB ABCDABCDABDE";
-        String str2 = "ABCDABD";
-        //String str2 = "BBC";
-        int[] next = kmpNext("ABCDABD"); //[0, 1, 2, 0]
-        System.out.println("next=" + Arrays.toString(next));
-        int index = kmpSearch(str1, str2, next);
-        System.out.println("index=" + index); // 15 了
 
-    }
 
     /**
      * @param str1 源字符串
