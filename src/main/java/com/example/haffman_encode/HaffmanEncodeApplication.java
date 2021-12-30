a27989d445574b2d2996b3cf4a03c317ba6e265e
SWQXDBA <983110853@qq.com>
2021-12-31 00:16:22 +0800

    1

--- src/main/java/com/example/haffman_encode/HaffmanEncodeApplication.java
+++ src/main/java/com/example/haffman_encode/HaffmanEncodeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HaffmanEncodeApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {//
         SpringApplication.run(HaffmanEncodeApplication.class, args);
     }
 
